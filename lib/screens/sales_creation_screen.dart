f97d9716bb17cc9479c6f09b3a19ebe367472d4d
kosba <mohamed.kosba96@gmail.com>
2020-10-04 17:33:55 +0200

    باخد بيانات من المستخدم - عرض السعر بالقرش

--- lib/screens/sales_creation_screen.dart
+++ lib/screens/sales_creation_screen.dart
@@ -10,14 +10,16 @@ class SalesCreationScreen extends StatefulWidget {
 
 class _SalesCreationScreenState extends State<SalesCreationScreen> {
 
-  int clientID = 00001;
+  int clientID = 10001;
   String clientName = '';
   String clientAddress = '';
   String clientPhoneNumber = '';
-  String itemAmount = '';
-  String itemPrice = '';
-  String totalPrice = '';
+  String itemAmount ;
+  String itemPrice = '36.17';
+  String totalInputPrice;
+  double totalPrice;
   String itemDescription = '';
+  String priceToRender;
 
 
 
@@ -91,12 +93,26 @@ class _SalesCreationScreenState extends State<SalesCreationScreen> {
               height: 24.0,
             ),
             TextField(
+              enabled: false,
               textAlign: TextAlign.center,
               keyboardType: TextInputType.number,
               onChanged: (value){
-                totalPrice = value;
+                itemPrice = value;
               },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'اكتب اجمالي سعر الفاتوره هنا'),
+              decoration: kTextFieldDecoration.copyWith(hintText: 'اكتب  سعر الصنف هنا'),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 24.0,
+            ),
+            TextField(
+              enabled: false,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              onChanged: (value){
+                totalInputPrice = value;
+              },
+              decoration: kTextFieldDecoration.copyWith(hintText: 'اكتب اجمالي الفاتوره هنا'),
             ),
             SizedBox(
               width: double.infinity,
@@ -118,6 +134,20 @@ class _SalesCreationScreenState extends State<SalesCreationScreen> {
             ),
             MainBTN(btnName: 'انشاء فاتوره',
               onClick: (){
+              clientID++;
+              double amount = double.parse(itemAmount);
+              double price = double.parse(itemPrice);
+              totalPrice = (amount * price);
+              priceToRender = totalPrice.toStringAsFixed(3);
+
+              print('رقم العميل : $clientID');
+              print('اسم العميل: $clientName');
+              print('عنوان العميل: $clientAddress');
+              print('رقم هاتف العميل: $clientPhoneNumber');
+              print('ملاحظات: $itemDescription');
+              print('سعر الصنف: $itemPrice جنيه');
+              print('كميه الصنف: $itemAmount قطعه');
+              print('اجمالي الفاتوره: $priceToRender جنيه');
                 // Navigator.pushNamed(context, SalesCreationScreen.id);
               },),
           ],
